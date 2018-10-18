1a3c525b2d7fe49abc8ade813873a68fdca5e208
user <user@DESKTOP-INQMPH2>
2018-10-18 19:06:41 +0800

    WCH ADD

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using ConsoleApplication1.model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = findOpenData();
+            showOpenData(nodes);
+            Console.ReadKey();
+        }
+
+        static List<OpenData> findOpenData()
+        {
+            List<OpenData> result = new List<OpenData>();
+
+            var xml = XElement.Load(@"deadfactory.xml");
+            var nodes = xml.Descendants("row_item").ToList();
+            /*
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+                OpenData item = new OpenData();
+
+                item.名稱 = getValue(node, "名稱");
+                item.鄉鎮市別 = getValue(node, "鄉鎮市別");
+                item.地址 = getValue(node, "地址");
+                item.聯絡單位 = getValue(node, "聯絡單位");
+                item.電話 = getValue(node, "電話");
+
+
+                result.Add(item);
+
+            }*/
+            nodes.ToList()
+                .ForEach(node =>
+            {
+                OpenData item = new OpenData();
+
+                item.名稱 = getValue(node, "名稱");
+                item.鄉鎮市別 = getValue(node, "鄉鎮市別");
+                item.地址 = getValue(node, "地址");
+                item.聯絡單位 = getValue(node, "聯絡單位");
+                item.電話 = getValue(node, "電話");
+                result.Add(item);
+            });
+            return result;
+        }
+
+        private static string getValue(XElement node, string header)
+        {
+            return node.Element(header)?.Value?.Trim();
+        }
+
+        public static void showOpenData(List<OpenData> nodes)
+        {
+            Console.WriteLine(string.Format("登記在宜蘭縣的殯葬設施，總共有{0}家", nodes.Count));
+            nodes.GroupBy(node => node.名稱).ToList()
+                .ForEach(group =>
+                {
+                    var key = group.Key;
+                    var allDatas = group.ToList();
+                    var message = $"名稱為:{key}的殯葬設施有{allDatas.Count()}家";
+                    Console.WriteLine(message);
+                });
+
+
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/model/OpenData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1.model
+{
+    class OpenData
+    {
+        public string 名稱 { get; set; }
+
+        public string 鄉鎮市別 { get; set; }
+
+        public string 地址 { get; set; }
+
+        public string 聯絡單位 { get; set; }
+
+        public string 電話 { get; set; }
+
+    }
+}
